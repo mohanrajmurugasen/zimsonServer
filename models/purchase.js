1c6caeb1ed23ae318470285e7041cc4b06985ec0
mohanrajmurugasen <mohanraj1711999@gmail.conm>
2022-04-22 14:51:50 +0530

    ds

--- models/purchase.js
+++ models/purchase.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     phone: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     email: {
